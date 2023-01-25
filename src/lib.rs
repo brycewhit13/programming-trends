791aed068910afcb2b70fd54b3ce9a3ca25798aa
Bryce Whitney <brycewhit13@gmail.com>
2023-01-24 20:03:23 -0500

    updated lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,7 @@
 // Import crates
 use reqwest;
 use rtrend;
+
+pub fn get_programming_lang_comparison(){
+    // List of programming languages: Python, Rust, C++, C, javascript, java, Cobol, Swift, PHP, Go
+}
