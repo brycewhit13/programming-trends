4459ff2a8482d8769657be91a26b9cb8509b681a
Bryce Whitney <brycewhit13@gmail.com>
2023-02-01 10:13:40 -0500

    Plotting the results as another option

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@
 
 // Import crates
 use rtrend::{Client, Country, Keywords, RegionInterest};
-use serde_json::{json, Value};
+use serde_json::{Value};
+use plotly::{Plot, Bar};
+use plotly::common::Title;
 
-pub fn get_programming_lang_comparison() {
+pub fn get_rust_popularity() -> Value {
     // vector of programming languages
-    let program_langs = vec!["Python"];
+    let program_langs = vec!["Rust"];
 
     // Make the request with rtrend
     let keywords = Keywords::new(program_langs);
@@ -15,12 +17,13 @@ pub fn get_programming_lang_comparison() {
     let client = Client::new(keywords, country).build();
 
     let results = RegionInterest::new(client).get();
-
-    // print and format the results
-    format_result(json!(results));
+    
+    // Return the results
+    results
 }
 
-fn format_result(result: Value) {
+
+pub fn print_result(result: Value) {
     // Loop through each state - 51 because DC is included
     for st in 0..51 {
         // Access the state and value data
@@ -30,4 +33,23 @@ fn format_result(result: Value) {
         // return formatted results
         println!("{}: {}", state, num_searches);
     }
+}
+
+pub fn plot_result(result: Value) {
+    // Get vector of states and values
+    let mut states: Vec<Value> = Vec::new();
+    let mut values: Vec<Value> = Vec::new();
+
+    for st in 0..51 {
+        states.push(result["default"]["geoMapData"][st]["geoName"].clone());
+        values.push(result["default"]["geoMapData"][st]["value"][0].clone());
+    }
+
+    // Plot the results
+    let mut plot = Plot::new();
+    let trace = Bar::new(states, values);
+    plot.add_trace(trace);
+
+    // Show the plot
+    plot.write_html("output.html");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
-// improt functions from lib.rs
-use programming_trends::get_programming_lang_comparison;
+// import crates and functions from lib.rs
+//use clap::Parser;
+use serde_json::{json, Value};
+use programming_trends::get_rust_popularity;
+use programming_trends::print_result;
+use programming_trends::plot_result;
+
+/* 
+// Add extended help
+#[derive(Parser, Debug)]
+#[clap(author = "Bryce Whitney", 
+        version = "4.1.1", 
+        about = "A command-line tool that looks at the popularity of different programming languages", 
+        after_help = "Example: cargo run --language [LANGUAGE]")]
+struct Cli {
+   // The programming language
+   #[arg(short, long)]
+   language: &str,
+} */
 
 pub fn main() {
-    get_programming_lang_comparison();
+    //let args = Cli::parse();
+    // Get the comparison
+    let results: Value = get_rust_popularity();
+
+    // Print the results
+    print_result(json!(results));
+
+    // PLot the results
+    plot_result(json!(results));
+
 }
